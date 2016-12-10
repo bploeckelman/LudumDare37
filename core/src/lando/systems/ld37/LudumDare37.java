7ab64c792bf98c41cead35c1bfd52976475a76db
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-12-10 13:01:25 -0600

    Add assets stuff

--- core/src/lando/systems/ld37/LudumDare37.java
+++ core/src/lando/systems/ld37/LudumDare37.java
@@ -2,42 +2,45 @@ package lando.systems.ld37;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import lando.systems.ld37.screens.BaseScreen;
 import lando.systems.ld37.screens.GameScreen;
+import lando.systems.ld37.utils.Assets;
 
 public class LudumDare37 extends ApplicationAdapter {
-	SpriteBatch batch;
-	BaseScreen screen;
+
 	public static LudumDare37 game;
 
+	SpriteBatch batch;
+	BaseScreen screen;
 
 	@Override
 	public void create () {
+	    Assets.load();
+	    float progress = 0f;
+	    do {
+	    	progress = Assets.update();
+		} while (progress != 1f);
 		game = this;
-		batch = new SpriteBatch();
-		setScreen(new GameScreen());
+
+		batch = Assets.batch;
+        setScreen(new GameScreen());
 	}
 
 	@Override
 	public void render () {
 		float dt = Math.min(Gdx.graphics.getDeltaTime(), 1f / 30f);
-
 		screen.update(dt);
-
 		screen.render(batch);
-
-
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		Assets.dispose();
 	}
 
 	public void setScreen(BaseScreen newScreen){
 		screen = newScreen;
 	}
+
 }
